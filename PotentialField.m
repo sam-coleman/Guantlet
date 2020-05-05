55efdf51365d7b85e587f18c33fd9105fc4ae93e
sam-coleman <60279462+sam-coleman@users.noreply.github.com>
2020-05-05 15:07:10 -0400

    fixed some scaling problems
    still not right? looks like shrek?

--- PotentialField.m
+++ PotentialField.m
@@ -20,14 +20,16 @@ yint_sink = all_b(1,9:11);
 %v = ln(sqrt((x-2)^2+y^2)
 
 %intialize variables
-v_all = [];
-v_all_source = 0;
+v_all = 0;
 [x,y]=meshgrid(-1.5:0.05:2.5,-3.5:0.05:1);  %Overall area of gauntlet
 x_array = -1.5:0.05:2.5;
 y_array = -3.5:0.05:1;
 v_source = 0;
+v_sink = 0;
 gx_source=0;
 gy_source=0;
+gx_sink = 0;
+gy_sink = 0;
 n = 1;
 
 syms x_sym y_sym
@@ -38,7 +40,7 @@ hold on
 while n<9
     %reset v_source for each line
     v_source = 0;
-    
+    num_points = 0;
     %if statement determines which endpoint is further left (we iterate
     %left to right based on x values)
     if all_endpts(n,1) > all_endpts(n,2)
@@ -50,7 +52,8 @@ while n<9
             v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2)); %add sources
             gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2)); %gradient vector in ihat dir
             gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2)); %gradient vector in jhat dir
-            %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2)); 
+            %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+            num_points = num_points + 1;
         end
     else
         for a = all_endpts(n,1):0.001:all_endpts(n,2)
@@ -60,38 +63,57 @@ while n<9
             gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
             gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
             %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+            num_points = num_points + 1;
         end
     end
-    %scale everything so same order of mag (doing this because v_source
-    %first iteration is in order of magnitude of -10,000 and other values
-    %are mag of 1, 10, 100
-    mean2(v_source) %find average of v_source
-    %tries to scale values accordingly
-    if abs(mean2(v_source)) < 10
-        v_source_scaled = v_source .* 1000;
-    elseif abs(mean2(v_source)) < 100
-        v_source_scaled = v_source .* 10;
-    elseif abs(mean2(v_source)) > 1000
-        v_source_scaled = v_source ./ 10;
-    elseif abs(mean2(v_source)) > 10000
-        v_source_scaled = v_source ./ 100;
-    elseif abs(mean2(v_source)) > 100000
-        v_source_scaled = v_source ./ 1000;
-    else
-        v_source_scaled = v_source;
-    end
-    mean2(v_source_scaled) %;et's us see average of scaled values
-    v_all_source = v_all_source + v_source_scaled; %update the combination of all v_source_all
+    v_source_scaled = v_source ./ num_points;
+    v_all = v_all + v_source_scaled; %update the combination of all v_source_all
     %v_all_source = [v_all_source v_source];
 
     %v_all = [v_all, v_sym];
     n = n+1;
 end
+n = 9;
+while n < 12
+ %reset v_source for each line
+    v_sink = 0;
+    num_points = 0;
+    %if statement determines which endpoint is further left (we iterate
+    %left to right based on x values)
+    if all_endpts(n,1) > all_endpts(n,2)
+        %for loop creates line of evenly spaced sources for each ransac
+        %line
+        for a = all_endpts(n,2):0.001:all_endpts(n,1)
+            a; %unsurpress to see it iterating
+            y_int = all_m(1,n)*a + all_m(1,n); %find corresponding y value for each x
+            v_sink = v_sink + log(sqrt((x-a).^2 + (y-y_int).^2)); %add sources
+            gx_sink = gx_sink+((x-a)./((x-a).^2 + (y-y_int).^2)); %gradient vector in ihat dir
+            gy_sink = gy_sink+((y-y_int)./((x-a).^2 + (y-y_int).^2)); %gradient vector in jhat dir
+            %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+            num_points = num_points + 1;
+        end
+    else
+        for a = all_endpts(n,1):0.001:all_endpts(n,2)
+            a;
+            y_int = all_m(1,n)*a + all_m(1,n);
+            v_sink = v_sink + log(sqrt((x-a).^2 + (y-y_int).^2));
+            gx_sink = gx_sink+((x-a)./((x-a).^2 + (y-y_int).^2));
+            gy_sink = gy_sink+((y-y_int)./((x-a).^2 + (y-y_int).^2));
+            %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+            num_points = num_points + 1;
+        end
+    end
+    v_sink_scaled = v_sink ./ num_points;
+    v_all = v_all + v_sink_scaled; %update the combination of all v_source_all
+    %v_all_source = [v_all_source v_source];
+
+    %v_all = [v_all, v_sym];
+    n = n+1;    
+end
 
 %v_all_simple = simplify(v_all);
 %v_values = double(subs(v_all_simple,[x_sym,y_sym],[x_array,y_array]));
 
-contour(x,y,v_all_source,'k','ShowText','On')
-plot(all_endpts(n,1:2),all_endpts(n,3:4),'*')
+contour(x,y,v_all,'k','ShowText','On')
 axis equal
 hold off
