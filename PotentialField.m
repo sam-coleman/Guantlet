96abfe0bafd47474d6235a7a8ca61b82066db467
reisenbud <ruby.eisenbud@gmail.com>
2020-05-05 10:44:52 -0400

    equations for potential field

--- PotentialField.m
+++ PotentialField.m
@@ -19,31 +19,48 @@ yint_sink = all_b(1,9:11);
 
 %v = ln(sqrt((x-2)^2+y^2)
 
+v_all = [];
+v_all_source = [];
+
 [x,y]=meshgrid(-1.5:0.05:2.5,-3.5:0.05:1);  %Overall area of gauntlet
 v_source = 0;
 gx_source=0;
 gy_source=0;
 
-if all_endpts(1,1) > all_endpts(1,2)
-    for a = all_endpts(1,2):0.001:all_endpts(1,1)
-        a
-        y_int = m_source(1,1)*a + yint_source(1,1);
-        v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
-        gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
-        gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
-    end
-else
-    for a = all_endpts(1,1):0.001:all_endpts(1,2)
-        a
-        y_int = m_source(1,1)*a + yint_source(1,1);
-        v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
-        gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
-        gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
-    end
-end
+n = 1;
+
+syms x_sym y_sym
+
 figure
 hold on
-contour(x,y,v_source,'k','ShowText','On')
-plot(all_endpts(1,1:2),all_endpts(1,3:4),'*')
+while n<9
+if all_endpts(n,1) > all_endpts(n,2)
+    for a = all_endpts(n,2):0.001:all_endpts(n,1)
+        a
+        y_int = m_source(1,n)*a + yint_source(1,n);
+        v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
+        gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
+        gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
+        v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+    end
+else
+    for a = all_endpts(n,1):0.001:all_endpts(n,2)
+        a
+        y_int = m_source(1,n)*a + yint_source(1,n);
+        v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
+        gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
+        gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
+        v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+    end
+end
+v_all_source = [v_all_source v_source];
+v_all = [v_all, v_sym];
+n = n+1;
+end
+
+v_values = double(subs(v_all,[x_sym,y_sym],[x,y]));
+
+contour(x,y,v_values,'k','ShowText','On')
+plot(all_endpts(n,1:2),all_endpts(n,3:4),'*')
 axis equal
 hold off
