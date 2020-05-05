d0192c3efb2f5328109d434224c5c37e9c8a3448
reisenbud <ruby.eisenbud@gmail.com>
2020-05-05 11:15:44 -0400

    Update PotentialField.m

--- PotentialField.m
+++ PotentialField.m
@@ -23,6 +23,8 @@ v_all = [];
 v_all_source = [];
 
 [x,y]=meshgrid(-1.5:0.05:2.5,-3.5:0.05:1);  %Overall area of gauntlet
+x_array = -1.5:0.05:2.5;
+y_array = -3.5:0.05:1;
 v_source = 0;
 gx_source=0;
 gy_source=0;
@@ -41,7 +43,7 @@ if all_endpts(n,1) > all_endpts(n,2)
         v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
         gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
         gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
-        v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+        %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
     end
 else
     for a = all_endpts(n,1):0.001:all_endpts(n,2)
@@ -50,17 +52,18 @@ else
         v_source = v_source - log(sqrt((x-a).^2 + (y-y_int).^2));
         gx_source = gx_source-((x-a)./((x-a).^2 + (y-y_int).^2));
         gy_source = gy_source-((y-y_int)./((x-a).^2 + (y-y_int).^2));
-        v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
+        %v_sym = -log(sqrt((x_sym-a).^2 + (y_sym-y_int).^2));
     end
 end
 v_all_source = [v_all_source v_source];
-v_all = [v_all, v_sym];
+%v_all = [v_all, v_sym];
 n = n+1;
 end
 
-v_values = double(subs(v_all,[x_sym,y_sym],[x,y]));
+%v_all_simple = simplify(v_all);
+%v_values = double(subs(v_all_simple,[x_sym,y_sym],[x_array,y_array]));
 
-contour(x,y,v_values,'k','ShowText','On')
+contour(x,y,v_all_source,'k','ShowText','On')
 plot(all_endpts(n,1:2),all_endpts(n,3:4),'*')
 axis equal
 hold off
