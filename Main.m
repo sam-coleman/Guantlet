e14376470cea69cf243d223eff78054937e505cf
sam-coleman <60279462+sam-coleman@users.noreply.github.com>
2020-05-06 13:06:42 -0400

    cleaned repo and added report

--- Main.m
+++
@@ -1,5 +0,0 @@
-%goal of this assignment - use LIDAR data to create MAP. Use those points
-%are very high mountain peak things. We need to get to the bucket of
-%benevolence which is the lowest point. Through gradient descent. 
-
-%collect LIDAR data
